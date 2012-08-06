7d34bfcb2135ba7915a5eeaa57303350ff20ede6
Monica Peters <monigarr@yahoo.com>
2012-08-05 21:45:49 -0400

    AOC1 Week 3 Project added, working correctly, complete.

---
+++ week3/aoc1_week3/aoc1_week3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aoc1_week3
+//
+//  Created by Monica Peters on 8/5/12.
+//  Copyright (c) 2012 Web & Mobile Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
